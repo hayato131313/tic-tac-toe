183f7b78e3b1ac643d6f39878c936041bb82ebb5
hayato131313 <hayato20010621@gmail.com>
2025-07-16 21:18:48 +0900

    Fix onPlay not a function error

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,12 +5,8 @@ function Square({value,onSquareclick}) {
   return <button className="square" onClick={onSquareclick}>{value}</button>;
 }
 
-function Board(){
-const[squares,setSquare]=useState(Array(9).fill(null)); 
-const[xIsNext,setXIsNext]=useState(true)
-
+function Board({xIsNext,squares,onPlay}){
 const winner =calculateWinner(squares);
-
 let status;
 if (winner){status='The Winner:'+winner;}
 else{status='Next Player:'+(xIsNext?'X':'O');}
@@ -21,8 +17,7 @@ if (calculateWinner(squares)||squares[i]){return;}
 const nextSquares=squares.slice();
 if (xIsNext){nextSquares[i]='X';}
 else{nextSquares[i]='O'}
-setSquare(nextSquares)
-setXIsNext(!xIsNext)
+onPlay(nextSquares);
 }
 
 return(<>
@@ -45,7 +40,49 @@ return(<>
 
 </>);}
 
-export default function Game(){}
+export default function Game(){
+const[history,setHistory]=useState([Array(9).fill(null)]);
+const[currentMove,setCurrentMove]=useState(0);
+const xIsNext=currentMove%2===0;
+const currentSquares=history[currentMove];
+
+function handleplay(nextSquares){
+const nextHistory=[...history.slice(0,currentMove+1),nextSquares]
+setHistory(nextHistory);
+setCurrentMove(nextHistory.length-1);
+}
+
+function jumpTo(nextMove){
+setCurrentMove(nextMove);
+}
+
+const moves=history.map((squares,move)=>{
+let description;
+if (move>0){description='Go to move #'+move;}
+else {description='Go to game start'}
+return(
+<li key={move}>
+<button onClick={()=>jumpTo(move)}>{description}</button>
+</li>
+)});
+
+return(
+    <div className="game">
+      <div className="game-board">
+        
+        <Board 
+        xIsNext={xIsNext}
+        squares={currentSquares}
+        
+        onPlay={handleplay}/>
+      </div>
+
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+};
 
 function calculateWinner(squares) {
   const lines = [
